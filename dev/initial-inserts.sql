cf80e6ac5549b4554fc2de0ca12c74d903dfcb9e
Ana <anaclaraanderle2003@gmail.com>
2025-05-01 21:57:52 -0300

    chore: Adding dockerfile, docker compose and initial inserts

---
+++ dev/initial-inserts.sql
@@ -0,0 +1,3 @@
+INSERT INTO system_user (id, name, birthday, cellphone, email, password, deleted)
+VALUES (nextval('system_user_seq'), 'Ana Clara', '2003-09-25', '51998102883', 'anaclaraanderle2003@gmail.com',
+        '12345678', false);
