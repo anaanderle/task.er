c1fb8b14a861576553af1785477454c27a0fb2ec
Carolina-S-Santos <carolina.sistemax@gmail.com>
2025-06-16 23:56:45 -0300

    feat/add google token table

--- src/main/java/com/uni/task/er/service/GoogleCalendarService.java
+++ src/main/java/com/uni/task/er/service/GoogleCalendarService.java
@@ -5,10 +5,12 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.EventDateTime;
+import com.uni.task.er.config.DatabaseDataStoreFactory;
+import com.uni.task.er.repository.GoogleTokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -21,24 +23,23 @@ import java.util.Date;
 public class GoogleCalendarService {
     private static final String APPLICATION_NAME = "Task.er";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    @Autowired
+    private GoogleTokenRepository googleTokenRepository;
 
     public Calendar getCalendarService() throws Exception {
         File credentialsFile = new File("credentials.json"); // Busca no diretório de trabalho do container
         if (!credentialsFile.exists()) {
             throw new RuntimeException("Arquivo credentials.json não encontrado no diretório de trabalho!");
         }
-        File tokensDir = new File(TOKENS_DIRECTORY_PATH);
-        if (!tokensDir.exists()) {
-            tokensDir.mkdirs();
-        }
+        
         FileInputStream in = new FileInputStream(credentialsFile);
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
                 GoogleNetHttpTransport.newTrustedTransport(), JSON_FACTORY, clientSecrets,
                 Collections.singletonList("https://www.googleapis.com/auth/calendar"))
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setDataStoreFactory(new DatabaseDataStoreFactory(googleTokenRepository))
                 .setAccessType("offline")
                 .build();
 
